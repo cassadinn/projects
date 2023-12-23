ff022c9725945d7f5f993a34499e4d984b07a7c8
cassadinn <143659213+cassadinn@users.noreply.github.com>
2023-12-23 15:58:37 +0100

    Update voice recorder.py

--- voice recorder/voice recorder.py
+++ voice recorder/voice recorder.py
@@ -6,17 +6,14 @@ import wave
 import keyboard
 
 def record_voice_until_enter(filename):
-    # Set the sampling frequency and channels
-    fs = 44100  # 44.1 kHz
-    channels = 2  # Stereo
+    fs = 44100 
+    channels = 2
 
-    # Initialize recording buffer
     recording = []
 
     print("Press Enter to start recording...")
     keyboard.wait("enter")
 
-    # Record audio until Enter is pressed again
     with sd.InputStream(samplerate=fs, channels=channels, dtype=np.int16) as stream:
         stream.start()
 
@@ -27,18 +24,15 @@ def record_voice_until_enter(filename):
             if keyboard.is_pressed("enter"):
                 break
 
-    # Save the recorded audio to a WAV file
     with wave.open(filename, 'wb') as wf:
         wf.setnchannels(channels)
-        wf.setsampwidth(2)  # 16-bit audio
+        wf.setsampwidth(2)  
         wf.setframerate(fs)
         wf.writeframes(np.array(recording).tobytes())
 
 if __name__ == "__main__":
-    # Set the filename for the recorded audio
-    output_filename = "recorded_voice.wav"
+    output_filename = "recorded_voice.wav" #file name
 
-    # Record the voice until Enter is pressed
     record_voice_until_enter(output_filename)
 
     print(f"Recording saved as {output_filename}")
